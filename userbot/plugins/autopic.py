e1c4d96d7ffb38e6d31bbe050c0a918230a4d43b
cirakata <63696553+cirakata@users.noreply.github.com>
2020-04-15 06:02:48 +0000

    Update autopic.py

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -21,11 +21,11 @@ async def autopic(event):
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
         file_test = im.rotate(counter, expand=False).save(photo, "PNG")
-        current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \nnj7 assistant\n°°°°°°°°°°°°°°°°°°°°\n  Time: %H:%M \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+        current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \nnj7 assistant\n°°°°°°°°°°°°°°°°°°°°\n\n  Time: %H:%M \n  Date: %d.%m.%y \n\n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((150, 500), current_time, font=fnt, fill=(255, 255, 255))
+        drawn_text.text((500, 500), current_time, font=fnt, fill=(255, 255, 255))
         img.save(photo)
         file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
