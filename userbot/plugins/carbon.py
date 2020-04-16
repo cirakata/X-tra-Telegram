ab3dd97b84e3acca3080eb57ce34ef817ce26ffe
cirakata <63696553+cirakata@users.noreply.github.com>
2020-04-16 20:19:30 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -99,7 +99,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="<< @assistant_nj7 's `secret massage >>`\n\n**Colour Scheme: **`{}`".format(color_name),
+         caption="<< @assistant_nj7 's `secret massage` >>\n\n**Colour Scheme: **`{}`".format(color_name),
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
